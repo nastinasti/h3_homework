c9f3a19677bca4e77fc944f271e13a8fbb4a997e
Anastasiia Konvisarova <nastia.konvisarova1@gmail.com>
2020-12-09 20:37:07 +0200

    Ping Pong Pipe

--- hw9/ping_pong_pipe.py
+++ hw9/ping_pong_pipe.py
@@ -1,20 +1,27 @@
 from multiprocessing import Process, Pipe
 from time import sleep
 from os import getpid
+from log_orm import logger
+
 
 def ponger(receiver, sender, response):
   while True:
-
-  # receive a message
-
-  # print it as f"Process{getpid()} got message: {msg}"
-  # sleep before responding
-  # send response message back
+    receiver.send(response)
+    logger.info(f"Process {getpid()} got message: {response}")
+    sleep(2)
+    sender.send(response)
 
 
 if __name__ == "__main__":
-# use pipe
-# create 2 processes that will use ponger, give them different sides of pipes
-# they also need a specific message (either ping or pong)
-# start both processes
-# initiate ping-pong by sending first message to one of the pipes
+  receiver, sender = Pipe()
+  receiver_process = Process(target=ponger, args=(receiver, sender, 'Pong'))
+  sender_process = Process(target=ponger, args=(sender, receiver, 'Ping'))
+
+  receiver_process.start()
+  sender_process.start()
+  print(receiver.recv())
+  print(sender.recv())
+  receiver_process.join()
+  sender_process.join()
+  receiver_process.close()
+  sender_process.close()
