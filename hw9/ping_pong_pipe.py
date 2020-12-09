6667e16066751de37ff32a0e46e9ba0d3d0a2bd5
Anastasiia Konvisarova <nastia.konvisarova1@gmail.com>
2020-12-09 19:31:11 +0200

    Save the progam

---
+++ hw9/ping_pong_pipe.py
@@ -0,0 +1,20 @@
+from multiprocessing import Process, Pipe
+from time import sleep
+from os import getpid
+
+def ponger(receiver, sender, response):
+  while True:
+
+  # receive a message
+
+  # print it as f"Process{getpid()} got message: {msg}"
+  # sleep before responding
+  # send response message back
+
+
+if __name__ == "__main__":
+# use pipe
+# create 2 processes that will use ponger, give them different sides of pipes
+# they also need a specific message (either ping or pong)
+# start both processes
+# initiate ping-pong by sending first message to one of the pipes
