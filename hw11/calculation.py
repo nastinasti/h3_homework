3bf6bfb7d86f1e1acd984fb83137b6297b809a9d
Anastasiia Konvisarova <nastia.konvisarova1@gmail.com>
2020-12-17 19:21:06 +0200

    make it functional

--- hw11/calculation.py
+++ hw11/calculation.py
@@ -1,31 +1,52 @@
 import re
+from collections import Counter
 
 class Calculation:
 
     def result(self, res):
-
-        result_list = list()
-        # while input_str != '=':
-        #     input_str = input("Please input: ")
-        #     res += input_str
-        # res = '220 % 10= '
         print(res)
         for item in res:
             if item == '=' or item == ' ':
                 res = res.replace('=', '')
-        match = re.split(r"\s*([%+*/-])\s*", res)
+        breckets_result = list()
+        c = Counter(res)
+        brekets_check = c['('] + c[')']
+        if (brekets_check) % 2 == 0:
+            sub_brackets = re.findall(r'\(([^\(\)]*)\)', res)
+            for item in sub_brackets:
+                match = re.split(r"[-+]?[.]?[\s]+?([)(%+*/-])?\s*", item)
+                if '*' in match:
+                    self.check_for_match(match, self._mult, '*')
+                if '/' in match:
+                    self.check_for_match(match, self._div, '/')
+                if '%' in match:
+                    self.check_for_match(match, self._persentage, '%')
+                if '+' in match:
+                    self.check_for_match(match, self._sum, '+')
+                if '-' in match:
+                    self.check_for_match(match, self._sub, '-')
+                breckets_result.append(match[0])
+        else:
+            raise ValueError
+        i = 0
+        for item in sub_brackets:
+            res = res.replace(item, breckets_result[i])
+            i = i+ 1
+        res = res.replace('(', '')
+        res = res.replace(')', '')
+        last_match = re.split(r"\s*([%+*/-])\s*", res)
 
-        if '*' in match:
-            self.check_for_match(match, self._mult, '*')
-        if '/' in match:
-            self.check_for_match(match, self._div, '/')
-        if '%' in match:
-            self.check_for_match(match, self._persentage, '%')
-        if '+' in match:
-            self.check_for_match(match, self._sum, '+')
-        if '-' in match:
-            self.check_for_match(match, self._sub, '-')
-        return float(''.join(match))
+        if '*' in last_match:
+            self.check_for_match(last_match, self._mult, '*')
+        if '/' in last_match:
+            self.check_for_match(last_match, self._div, '/')
+        if '%' in last_match:
+            self.check_for_match(last_match, self._persentage, '%')
+        if '+' in last_match:
+            self.check_for_match(last_match, self._sum, '+')
+        if '-' in last_match:
+            self.check_for_match(last_match, self._sub, '-')
+        return float(''.join(last_match))
 
     def check_for_match(self, match_list, func, sign):
         for item in match_list:
@@ -37,7 +58,6 @@ class Calculation:
                 times_result = func(a, b)
                 match_list.insert(match_list.index(item), '{:.2f}'.format(times_result))
                 match_list.pop(match_list.index(item))
-                print(match_list)
 
     def _sum(self, a, b):
         return a + b
@@ -56,5 +76,6 @@ class Calculation:
 
 if __name__ == '__main__':
     calc = Calculation()
-    print(calc.result())
+    res = '(-50 + 60 * 4)*2 - (90 - 1)'
+    print(calc.result(res))
 
