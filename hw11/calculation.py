85956ba277e686d8572be95d8e88f348ce4d6a75
Anastasiia Konvisarova <nastia.konvisarova1@gmail.com>
2020-12-20 16:37:22 +0200

    Deal with brackets issue

--- hw11/calculation.py
+++ hw11/calculation.py
@@ -13,44 +13,38 @@ class Calculation:
         breckets_result = list()
         c = Counter(res)
         brekets_check = c['('] + c[')']
+        print(f"amount of brackets pairs in the equation is {brekets_check/2}")
         pattern = r"([-+]?\d+\.\d+|\d+)([-+*/])"
-        if (brekets_check) % 2 == 0:
+        while (brekets_check)/2 != 0:
             sub_brackets = re.findall(r'\(([^\(\)]*)\)', res)
             for item in sub_brackets:
                 match = re.split(pattern, str(item).replace(' ', ''))
                 match = list(filter(None, match))
                 print(f"match: {match}")
-                if len(match) == 1:
-                    match = match[0]
-                    breckets_result = (float(match))
-                    print(match)
-                else:
-                    self.operation(match)
-                    breckets_result.append(float(match[0]))
-        else:
-            raise ValueError
-        i = 0
-        print(f"brekets_result: {breckets_result} type: {type(breckets_result)}")
-        for item in sub_brackets:
-            print(f"item: {item}")
-            if len(breckets_result) > 0:
-                res = res.replace(item, str(breckets_result[i]))
-                print("im here")
-                i = i + 1
-            if breckets_result is list and len(breckets_result) == 1:
-                print("im here")
-                res = res.replace(item, str(float(breckets_result[0])))
-            if breckets_result is float:
-                print("im here")
+                print(f"subbreckets are: {sub_brackets}")
+                self.operation(match)
+                breckets_result.append(float(match[0]))
+                res = self._ch_remove(res, res.find(str(item)) - 1)
+                res = self._ch_remove(res, res.find(str(item)) + len(item))
+                res = res.replace(item, str(match[0]))
+                print(f"im here. res after removing is {res}")
+            new_c = Counter(res)
+            brekets_check = new_c['('] + new_c[')']
+            print(f"amount of brackets pairs in the equation is {brekets_check / 2}")
+        if brekets_check == 0:
+            print(f"res = {res}")
+            print(res)
+            last_match = re.split(pattern, res.replace(' ', ''))
+            last_match = list(filter(None, last_match))
+            print(f"last match = {last_match}")
+            return (''.join(self.operation(last_match)))
+        # else:
+        #     raise ValueError
 
-        print(f"res = {res}")
-        res = res.replace('(', '')
-        res = res.replace(')', '')
-        print(res)
-        last_match = re.split(pattern, res.replace(' ', ''))
-        last_match = list(filter(None, last_match))
-        print(f"last match = {last_match}")
-        return (''.join(self.operation(last_match)))
+    def _ch_remove(self, string, index):
+        s = list(string)  # конвертируем в список
+        del s[index]  # удаляем элемент с индексом index
+        return "".join(s)
 
     def operation(self, last_match):
         while len(last_match) != 1:
@@ -73,19 +67,17 @@ class Calculation:
         return last_match
 
     def sum_sub_oper(self, match_list, func, sign):
-        # result = list()
-        # i = 0
-        # for item in match_list:
-        #     a = float(match_list[i])
-        #     b = float(match_list[i+2])
-        #     if match_list[i+1] == sign:
-        #         times_result = func(a, b)
-        #         result.append('{:.2f}'.format(times_result))
-        #         match_list.pop(i)
-        # print(f"match list : {match_list}")
-        # print(f"result: {result}")
-        # return result
-        pass
+        result = list()
+        i = 0
+        for item in match_list:
+            a = float(match_list[i])
+            b = float(match_list[i+2])
+            if match_list[i+1] == sign:
+                times_result = func(a, b)
+                result.append('{:.2f}'.format(times_result))
+                match_list.clear()
+                match_list.append('{:.2f}'.format(times_result))
+        print(f"match list : {match_list}")
 
 
     def check_for_match(self, match_list, func, sign):
@@ -120,6 +112,6 @@ class Calculation:
 
 if __name__ == '__main__':
     calc = Calculation()
-    res = '(5.0+6/54-60+10/8) /2 +9*10'
+    res = '((5.0 + 1) - (9-5)*100) /2 +9*10'
     print(calc.result(res))
 
